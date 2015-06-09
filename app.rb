34b67988ad452d0c7fbceb6a3dbfe37682787800
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-09 13:27:47 -0500

    Debugs tournament so that it works

--- app.rb
+++ app.rb
@@ -6,17 +6,28 @@ require_relative "tournament.rb"
 
 all_players = []
 
+#create players for the tournament
+
 ('a'..'g').each do |c|
   all_players.push(ComputerPlayer.new(c))
 end
 
+
+# all_players holds current players
+# when length of all_players is 1, you have a winner
+
 while all_players.length > 1
+  puts "\nPlaying a new round in the tournament!"
   new_tournament = Tournament.new(all_players)
   brackets = new_tournament.get_brackets
+  
   #play each bracket
-  brackets.each do |players_for_game|
+  brackets.each_with_index do |players_for_game, index|
+    puts "Round:\t#{index + 1}"
     winners = players_for_game
+  
     # loop until you get a winner for the bracket
+    # play_game may take a few games if there is a tie
     while winners.length > 1
       args = {}
       new_game = PaperRockScissorsGame.new(args)
@@ -25,14 +36,14 @@ while all_players.length > 1
       winners = g_driver.play_game  
     end
     #remove loser from all_players
-    binding.pry
+
     players_for_game.delete(winners[0])
     all_players.delete(players_for_game[0])
   end
-  puts "Playing a new round in the tournament!"
+  
 end
 
-puts "The ultimate winner is #{all_players}"
+puts "The ultimate winner is #{all_players[0].name}"
 
 # puts "\n\nNow testing by setting up my own computer players."
 #
